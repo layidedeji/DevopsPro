79309553ec28f24e2095d72244fae8ef9aaf1afe
Ubuntu <ubuntu@ip-10-0-11-20.ec2.internal>
2022-08-29 17:55:57 +0000

    This is my bash script

---
+++ bash.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+
+#UPDATE SERVER
+
+sudo apt update
+
+echo "UPDATING SERVER......"
+sleep 4sec
+echo
+echo
+
+#INSTALL DOCKER
+sudo apt install -y docker.io
+echo "INSTALLING DOCKER......."
+sleep 4
+echo
+echo
+
+#INSTALL JENKINS
+sudo apt install -y default-jre
+sudo apt install -y default-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+echo "INSTALLING JENKINS......"
+sleep 4
+echo
+echo
+
+#START JENKINS
+sudo systemctl start jenkins
+echo "STARTING JENKINS........"
+sleep 4
+echo
+echo
+
+
+#ADD JENKINS TO DOCEKER GROUP
+sudo chmod -aG docker jenkins
+echo "ADDING JENKINS TO DOCKER GROUP........"
+sleep 4
+echo
+echo
+echo "SUCCESSFULLY COMPLETED"
+
+
+
+
+
+
+
+
+
+
+
